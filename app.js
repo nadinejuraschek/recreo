5c89f77b4a8b22d23095a7a1e6debebf74706f48
Nadine Ju <nadine.jura@online.de>
2020-10-26 20:36:24 +0100

    playground creation form added

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+
+
 // ROUTES
 app.get('/', (req, res) => {
   res.render('home');
@@ -35,15 +39,19 @@ app.get('/playgrounds', async (req, res) => {
   res.render('playgrounds/index', { playgrounds });
 });
 
+app.get('/playgrounds/new', (req, res) => {
+  res.render('playgrounds/new');
+});
+
 app.get('/playgrounds/:id', async (req, res) => {
   const playground = await Playground.findById(req.params.id);
   res.render('playgrounds/show', { playground });
 });
 
-app.get('/create', async (req, res) => {
-  const playground = new Playground({ title: 'My Backyard' });
+app.post('/playgrounds', async (req, res) => {
+  const playground = new Playground(req.body.playground);
   await playground.save();
-  res.send(playground);
+  res.redirect(`/playgrounds/${playground._id}`);
 });
 
 app.listen(process.env.PORT, () => {
