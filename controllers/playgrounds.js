0d4ae39a1f69b9baec45cf45f29e3f500be099e6
Nadine Ju <nadine.jura@online.de>
2020-10-30 12:12:22 +0100

    controllers created

---
+++ controllers/playgrounds.js
@@ -0,0 +1,51 @@
+const Playground = require('../models/Playground');
+
+module.exports.getPlaygrounds = async (req, res) => {
+  const playgrounds = await Playground.find({});
+  res.render('playgrounds/index', { playgrounds });
+};
+
+module.exports.getSinglePlayground = async (req, res) => {
+  const playground = await Playground.findById(req.params.id)
+    .populate({
+      path: 'reviews',
+      populate: {
+        path: 'author',
+      },
+    })
+    .populate('author');
+  if (!playground) {
+    req.flash('error', 'This playground cannot be found.');
+    return res.redirect('/playgrounds');
+  }
+  res.render('playgrounds/show', { playground });
+};
+
+module.exports.showEditForm = async (req, res) => {
+  const playground = await Playground.findById(req.params.id);
+  res.render('playgrounds/edit', { playground });
+};
+
+module.exports.create = async (req, res, next) => {
+  const playground = new Playground(req.body.playground);
+  playground.author = req.user._id;
+  await playground.save();
+  req.flash('success', 'Successfully added a new playground!');
+  res.redirect(`/playgrounds/${playground._id}`);
+};
+
+module.exports.edit = async (req, res) => {
+  const { id } = req.params;
+  const editedPlayground = await Playground.findByIdAndUpdate(id, {
+    ...req.body.playground,
+  });
+  req.flash('success', 'Successfully updated this playground!');
+  res.redirect(`/playgrounds/${id}`);
+};
+
+module.exports.delete = async (req, res) => {
+  const { id } = req.params;
+  const deletedPlayground = await Playground.findByIdAndDelete(id);
+  req.flash('success', 'The playground has been removed.');
+  res.redirect('/playgrounds');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,8 @@
 const express = require('express'),
   passport = require('passport'),
   catchAsync = require('../utils/catchAsync'),
-  // ExpressError = require('../utils/ExpressError'),
-  User = require('../models/User'),
+  userController = require('../controllers/users'),
   router = express.Router({ mergeParams: true });
-
-// VALIDATION
-// const { userSchema } = require('../schemas.js');
-
-// ERROR HANDLING
-// const validateUser = (req, res, next) => {
-//   const { error } = reviewSchema.validate(req.body);
-//   if (error) {
-//     const msg = error.details.map(item => item.message).join(',');
-//     throw new ExpressError(msg, 400);
-//   } else {
-//     next();
-//   };
-// };
 
 // READ
 // LOGIN
@@ -31,44 +16,16 @@ router.get('/register', (req, res) => {
 });
 
 // LOGOUT
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', 'See you again soon!');
-  res.redirect('/');
-});
+router.get('/logout', userController.logout);
 
 // CREATE
 // LOGIN
 router.post('/login', passport.authenticate('local', {
   failureFlash: true,
   failureRedirect: '/login',
-}), catchAsync(async (req, res) => {
-  req.flash('success', 'Welcome back!');
-  const redirectUrl = req.session.returnTo || '/playgrounds';
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-}));
+}), catchAsync(userController.login));
 
 // REGISTER
-router.post('/register', catchAsync(async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-      if (err) return next(err);
-      req.flash('success', "Welcome to Recreo!");
-      res.redirect('/playgrounds');
-    });
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('/playgrounds');
-  };
-}));
-
-// UPDATE
-
-// DELETE
-
+router.post('/register', catchAsync(userController.register));
 
 module.exports = router;
