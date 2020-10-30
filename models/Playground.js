822f425209c23a00c3dd7487caa64f3ec0dc0a55
Nadine Ju <nadine.jura@online.de>
2020-10-30 22:58:18 +0100

    creating virtual thumbnail image of images

--- models/Playground.js
+++ models/Playground.js
@@ -3,14 +3,18 @@ const mongoose = require('mongoose'),
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+  return this.url.replace('/upload', '/upload/w_300');
+});
+
 const PlaygroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
