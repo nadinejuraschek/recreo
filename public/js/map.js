127b8b2d556d0b9fad8e73640772508c2490bcfe
Nadine Ju <nadine.jura@online.de>
2020-11-02 22:24:06 +0100

    moved map js out of template

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,13 @@
 mapboxgl.accessToken = mapToken;
+
+const geoDataArray = geoDataString.split(',');
+const coordinates = [parseFloat(geoDataArray[0]), parseFloat(geoDataArray[1])];
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [20, 20],
-  zoom: 8,
+  center: coordinates,
+  zoom: 10,
 });
 
-const marker = new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
+const marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
