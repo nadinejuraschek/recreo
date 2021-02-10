a92d937529acd0916a51cef0d19f78e5f5520d47
Nadine Ju <nadine.jura@online.de>
2021-02-10 18:00:54 +0100

    with image layout complete

--- client/src/layouts/ImageLayout/styles/ImageLayout.tsx
+++ client/src/layouts/ImageLayout/styles/ImageLayout.tsx
@@ -4,27 +4,33 @@ import styled from 'styled-components';
 export const HorizontalImage = styled.div.attrs(() => ({
   className: 'bg-image-horizontal',
 }))`
-  display: none;
   position: absolute;
-  top: 0;
   bottom: 0;
 
-  height: 100vh;
+  height: 40%;
   width: 100vw;
   z-index: -2;
 
   background-image: linear-gradient(#ffffff40, #ffffff40),
     url('https://images.unsplash.com/photo-1552537595-b30edb7afd9d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80');
-  background-position: center 35%;
+  background-size: cover;
+  background-position: center 25%;
 
   @media only screen and (min-width: 768px) {
-    display: block;
+    display: none;
   }
 `;
 
 export const HorizontalWave = styled.div.attrs(() => ({
   className: 'bg-image-horizontal',
 }))`
+  position: absolute;
+  bottom: 0;
+
+  height: 41%;
+  width: 100vw;
+  z-index: -2;
+
   & img {
     object-fit: cover;
     width: 100%;
