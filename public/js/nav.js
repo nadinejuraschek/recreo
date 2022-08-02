abeadaca1d7d82ea2313f2096d6da3d3189cc2be
nadinejuraschek <nadinejwebdev@gmail.com>
2022-08-02 19:13:58 +0200

    Fix: removed unused files

--- public/js/nav.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('nav-button');
-const menuDropdown = document.getElementById('nav-dropdown');
-
-menuBtn.addEventListener('click', () => {
-  menuDropdown.classList.toggle('hidden');
-});
