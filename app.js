00e9a741f458c31311be8d51451b41686740e241
nadinejuraschek <nadinejwebdev@gmail.com>
2022-08-18 17:58:28 +0200

    Update: updated mongoose to v6

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ const express = require('express'),
 dotenv.config();
 
 // DATABASE ================================================================
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGO_URI, {});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
